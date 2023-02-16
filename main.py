7ff0ba046aa2dd759a938f802bfeea9841253daf
Molu Tyagi <91013156+molutyagi@users.noreply.github.com>
2023-02-16 13:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
+import secrets
 
 app = Flask(__name__)
 app.app_context().push()
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
